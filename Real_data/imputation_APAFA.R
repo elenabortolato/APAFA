1f4b50e2b54167decf6461a04d59c19c2d27a5a8
Elena Bortolato <51712988+elenabortolato@users.noreply.github.com>
2025-05-06 16:39:41 +0200

    Update imputation_APAFA.R

--- Real_data/imputation_APAFA.R
+++ Real_data/imputation_APAFA.R
@@ -173,6 +173,48 @@ for (iter in iter:maxiter){
 
   }
 
+# RESULTS:
+memory.limit(999999)
+# for APAFA
+load("immune_res_imputation.RData")
+y=readRDS(file="immune_data.RDS")
+ 
+  
+MSE=rep(0,10000)
+iter=1                         
+for(iter in iter:10000) MSE[iter]=  sum((ris_y[iter,,]-y)^2)/30
+plot(MSE, type="l")
+
+boxplot((MSE[1:8000]))
+boxplot((MSE[10000:8000]))
+ 
+
+diff=matrix(0, 2000,30)
+for(iter in 8001:10000){
+for(ii in 1:30) diff[iter-8000,ii]=(y[idx_imp[ii,1],idx_imp[ii,2]]-ris_y[iter,idx_imp[ii,1],idx_imp[ii,2]]) 
+}
+boxplot((diff))
+
+pred=matrix(0, 2000,30)
+for(iter in 8001:10000){
+  for(ii in 1:30) pred[iter-8000,ii]=(ris_y[iter,idx_imp[ii,1],idx_imp[ii,2]]) 
+}
+yobs=matrix(0,1,30)
+ 
+  for(ii in 1:30) yobs[1,ii]=(y[idx_imp[ii,1],idx_imp[ii,2]]) 
+
+# 
+par(mfrow=c(1,2))
+
+plot(MSE, type="l", xlab="iteration")
+mean(MSE)
+boxplot((pred), ylab="prediction")
+points(1:30,yobs, col=2,pch=8,lwd=0.5,cex=01.8)
+
+
+
+                         
+
   
 
  
